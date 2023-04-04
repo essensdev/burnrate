6b6637f547659cb7d5ab352fd41c432a46e72022
0xShapeShifter <113918140+0xShapeShifter@users.noreply.github.com>
2023-04-04 08:32:47 +0300

    fix: uotdated preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,8 @@ import compress from "astro-compress";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind(), compress(), svelte()]
 });
